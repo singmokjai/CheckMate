636f4b9c4d35796b2973b9842ca488b94ada272d
Errol Rougier <errolrougier@dyn-209-2-209-187.dyn.columbia.edu>
2018-11-17 11:23:25 -0500

    Input Field into url

--- assets/js/liveFM.js
+++ assets/js/liveFM.js
@@ -3,11 +3,13 @@
 
 
 function displayArtistInfo() {
-    var queryURL = "http://ws.audioscrobbler.com/2.0/?method=artist.search&limit=1&artist=Cher&api_key=336b213548b87643810f891a0c694d83&format=json";
+    var queryURL = "http://ws.audioscrobbler.com/2.0/?method=artist.search&limit=1&artist=";
+    var artistName = $("#userInput").val().trim();
+    var liveApiKey = "&api_key=336b213548b87643810f891a0c694d83&format=json"
 
     $.ajax({
-        url: queryURL,
-        method: "GET"
+        url: queryURL + artistName + liveApiKey,
+        method: "GET",
     }).then(function (response) {
         console.log('response', response);
         console.log(queryURL);
